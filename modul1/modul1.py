14d88846867a733a4b7c9e26fdc79320f41b55d6
fajriyahyulia <fajriyahyulia22@student.uns.ac.id>
2026-08-30 21:13:20 +0700

    Create modul1.py

---
+++ modul1/modul1.py
@@ -0,0 +1,6 @@
+nama = input("Masukkan nama: ")
+umur = int(input("Masukkan umur: "))
+tahun_lahir = 2026 - umur
+
+print(f"Halo, {nama}! Senang berkenalan dengan Anda.")
+print(f"Perkiraan tahun lahir Anda adalah {tahun_lahir}.")
